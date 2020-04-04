9fde351d8cec305bcff3b076832ef44422d7ac10
runic-tears <51153795+runic-tears@users.noreply.github.com>
2020-04-04 12:38:06 +0400

    :cat: Совсем маленькое изменение, потому что могу

--- script.py
+++ script.py
@@ -117,6 +117,10 @@ async def main():
     for archive, archive_path in ARCHIVES:
         asyncio.create_task(exclude_fonts_from_archive(archive_path, archive))
 
+    input(
+        "\nПроцесс завершён, нажмите ВВОД (ENTER), чтобы закрыть программу..." if LOCALE in CYRILLIC_LOCALES
+        else "\nThe process is complete, press ENTER to close the program..."
+    )
 
 loop = asyncio.get_event_loop()
 loop.run_until_complete(main())
